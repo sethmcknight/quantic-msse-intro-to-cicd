442769464443883cbd979cc08a55b2a56f44fdb5
sethmcknight <7377802+sethmcknight@users.noreply.github.com>
2025-06-19 10:44:01 -0600

    Add comment to clarify usage of sayHi function

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ function sayHi(name) {
   return `Hello there ${name}`
 }
 // This is a simple function that takes a name as an argument and returns a greeting message.
+// It can be used in various applications to greet users or display messages.
 module.exports = sayHi
